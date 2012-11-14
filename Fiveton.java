277625826a55ae2234e88456a77d24d5452cf78a
codehoper <vsraje01@gmail.com>
2012-11-13 20:13:37 -0500

    Update Fiveton.java
    Fiveton class with double checking

--- Fiveton.java
+++ Fiveton.java
@@ -1,5 +1,64 @@
-package com.ub.interview;
 
 public class Fiveton {
+
+  private static int INSTANCE_CNT = 0;
+	private static int NO_INSTANCE = 5;
+	public static Fiveton INSTANCE[] = null;
+	public static Fiveton SINGLE_INSTANCE = null;
+
+	private Fiveton() {
+
+	}
+
+	public static Fiveton[] getInstance() {
+
+		if (INSTANCE == null) {
+			synchronized (Fiveton.class) {
+				for (int i = 0; i < NO_INSTANCE; i++) {
+					INSTANCE[i] = new Fiveton();
+				}
+			}
+		}
+
+		return INSTANCE;
+	}
+
+	/**
+	 * synchronized version
+	 * 
+	 * @return
+	 */
+	public static Fiveton getSingleInstance() {
+
+		if (SINGLE_INSTANCE == null) {
+			synchronized (Fiveton.class) {
+				if (INSTANCE_CNT < 5)
+					SINGLE_INSTANCE = new Fiveton();
+				INSTANCE_CNT++;
+			}
+		}
+
+		return SINGLE_INSTANCE;
+	}
+
+	/**
+	 * Double checking of null
+	 * http://www.ibm.com/developerworks/java/library/j-dcl/index.html
+	 * @return
+	 */
+	public static Fiveton getSingleInstanceDoubleCheck() {
+
+		if (SINGLE_INSTANCE == null) {
+			synchronized (Fiveton.class) {
+				if (SINGLE_INSTANCE == null) {
+					if (INSTANCE_CNT < 5)
+						SINGLE_INSTANCE = new Fiveton();
+					INSTANCE_CNT++;
+				}
+			}
+		}
+
+		return SINGLE_INSTANCE;
+	}
 
 }
